6d3aa924046213126f22dd0d08982ed2de972d9e
Luis Neira <lneira41086@gmail.com>
2025-05-29 01:04:26 -0400

    Add services & components

---
+++ src/components/RentalStack/index.tsx
@@ -0,0 +1,2 @@
+export { default } from "./RentalStack";
+export * from "./RentalStack";
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,21 @@
+import { Loader } from '@mantine/core';
+
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
+import useRentals from '@/hooks/useRentals';
+import RentalStack from '@/components/RentalStack';
+
 export function HomePage() {
+  const { rentals, isLoading } = useRentals();
+
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+
+      {isLoading && <Loader type="bars" mx="auto" my="lg" />}
+      <RentalStack rentals={rentals} />
     </>
   );
 }
